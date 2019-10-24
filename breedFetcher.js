ab56442632cb5aaf2069adb95b3e37c8ab049ea3
Michelle Siregar <michellesiregar@gmail.com>
2019-10-24 21:29:03 +0000

    refactored breedFetcher to take callback, created test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,18 @@
 const request = require('request');
-const name = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${name}`, (error, response, body) => {
-  if (error) {
-    throw error;
-  } else if (body === '[]') {
-    console.log('Breed not found');
-  } else {
-    const data = JSON.parse(body);
-    const desc = data[0].description;
-    console.log(desc);
-  }
-});
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error);
+    } else if (body === '[]') {
+      return callback('Breed does not exist');
+    } else {
+      const data = JSON.parse(body);
+      const desc = data[0].description.trim();
+      return callback(null, desc);
+    }
+  });
+};
+
+
+module.exports = { fetchBreedDescription };
